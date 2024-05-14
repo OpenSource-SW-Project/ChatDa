fb7fac37c5b036bfd9e1b34e78ae0dfa90a7e8ee
Jinho622 <aceho21@gmail.com>
2024-05-14 22:07:58 +0900

    [feat] talk entity에 생성일자 추가

--- src/main/java/Opensource_SW_Project/Project/domain/common/BaseEntity.java
+++ src/main/java/Opensource_SW_Project/Project/domain/common/BaseEntity.java
@@ -1,11 +1,13 @@
 package Opensource_SW_Project.Project.domain.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -14,8 +16,6 @@ import java.time.LocalDateTime;
 @Getter
 public abstract class BaseEntity {
     @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
