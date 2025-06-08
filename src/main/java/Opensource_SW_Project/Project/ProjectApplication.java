bcfaa5a3d45ac876ad20ae33021be73ff5ba7fce
Jinho622 <aceho21@gmail.com>
2025-06-08 20:15:14 +0900

    [chore] commit action 테스트7

--- src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
+++ src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
@@ -9,13 +9,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args); // error 해결!
+		SpringApplication.run(ProjectApplication.class, args);
 		// commit action 테스트 1
 		// commit action 테스트 2
 		// commit action 테스트 3
 		// ArrayIndexOutOfBoundsException 강제로 발생시키기
 		int[] testArray = new int[2];
-		System.out.println(testArray[0]); // ArrayIndexOutOfBoundsException 해결!
+		System.out.println(testArray[1]); // ArrayIndexOutOfBoundsException 해결!
 	}
 
 }
