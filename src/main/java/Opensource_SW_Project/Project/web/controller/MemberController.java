71cebd36563b258bdd10c9d49d0410b433179759
Jinho622 <aceho21@gmail.com>
2025-06-08 20:23:36 +0900

    [chore] commit action 테스트10

--- src/main/java/Opensource_SW_Project/Project/web/controller/MemberController.java
+++ src/main/java/Opensource_SW_Project/Project/web/controller/MemberController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RequestMapping("/users")
 @Slf4j
-public class MemberController {
+public class MemberController { // ArrayIndexOutOfBoundsException 해결!
 
     private final MemberCommandService memberCommandService;
     private final MemberQueryService memberQueryService;
