e4a98717c8474d7e5c662ba72805e4c37cfd51c6
Jinho622 <aceho21@gmail.com>
2025-06-09 00:56:35 +0900

    [chore] commit action 테스트12

--- src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
+++ src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
@@ -13,7 +13,7 @@ public class ProjectApplication {
 		// commit action 테스트 1
 		// commit action 테스트 2
 		// commit action 테스트 3
-		// ArrayIndexOutOfBoundsException 강제로 발생시키기 - 코드 블럭 저장
+		// ArrayIndexOutOfBoundsException 강제로 발생시키기 - 코드 블럭 저장!!
 		int[] testArray = new int[2];
 		System.out.println(testArray[1]); // ArrayIndexOutOfBoundsException 해결!
 	}
