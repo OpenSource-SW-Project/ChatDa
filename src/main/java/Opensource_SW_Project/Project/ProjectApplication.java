5630c38d8bad556e4bad881ab8d268b99494082a
Jinho622 <aceho21@gmail.com>
2025-06-09 16:06:21 +0900

    [chore] commit action 테스트13

--- src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
+++ src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
@@ -11,7 +11,7 @@ public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args); // error 해결!
 		// commit action 테스트 1
-		// commit action 테스트 2
+		// commit action 테스트 2 // commit하기!
 		// commit action 테스트 3
 		// ArrayIndexOutOfBoundsException 강제로 발생시키기 - 코드 블럭 저장!!
 		int[] testArray = new int[2];
