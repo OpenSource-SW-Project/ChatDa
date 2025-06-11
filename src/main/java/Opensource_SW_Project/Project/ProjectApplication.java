b7469756e057d06ae3b8e5bd47b8e9ce5daeed16
Jinho622 <aceho21@gmail.com>
2025-06-11 12:15:12 +0900

    [chore] commit action 테스트1

--- src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
+++ src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
@@ -3,13 +3,18 @@ package Opensource_SW_Project.Project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class ProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+
+		// 추가된 줄1
+		// 추가된 줄2
+		// 추가된 줄3
+		// 추가된 줄4
+		// 추가된 줄5
+		// 추가된 줄6
 	}
 
 }
