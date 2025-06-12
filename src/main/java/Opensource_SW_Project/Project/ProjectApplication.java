0a212092ae27edadbd7d9364d10d33ceb6086f8d
Jinho622 <aceho21@gmail.com>
2025-06-12 19:21:19 +0900

    [chore] commit action 테스트11

--- src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
+++ src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
@@ -5,13 +5,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class ProjectApplication {
-
-
-
-
-
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args); //!!
+		SpringApplication.run(ProjectApplication.class, args); // 해결됨!
+		int[] array = new int[2];
+		System.out.println(array[1]);
 	}
 
 }
