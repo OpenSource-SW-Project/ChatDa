116365f6cf54e3f5f62a86c3123fcede437727d7
Jinho622 <aceho21@gmail.com>
2025-06-12 20:37:03 +0900

    [chore] commit action 테스트13

--- src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
+++ src/main/java/Opensource_SW_Project/Project/ProjectApplication.java
@@ -5,11 +5,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class ProjectApplication {
+	// 추가된 줄1
+	// 추가된 줄2
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args); // 오류 해결!
 
-		// 추가된 줄1
-		// 추가된 줄2
 	}
 
 }
